1f464ec29a4b2b92f1ef5307caecc62b2a9ff368
laloYT <92411610+laloYT@users.noreply.github.com>
2023-07-18 16:31:18 -0600

    Add files via upload

---
+++ createVideo.py
@@ -0,0 +1,34 @@
+import os
+import cv2
+
+path = "images/"
+
+images = []
+
+
+for (os.dirlist (path); os.dirlist (archivo)) 
+
+if ext in ['.gif','.png','.jpg','.jpeg','jfif']:
+    file_name = path+"/"+file
+    print(file_name)
+    append()
+count 
+len (images)
+
+frame
+frame =cv2.imread(images[0])
+frame.shape
+
+size (width, height)
+print(size)
+
+exit cv2. VideoWriter():
+nameofvideo = Project.avi
+fourcc = cv2. VideoWriter_fourcc(*'DIVX')
+fps = 0.8
+size =size
+out = cv2.VideoWriter('proyect.avi',cv2.VideoWriter_fourcc(*'DIVX'), 0.8, size)
+for i in range(0, count-1)
+cv2.imread()
+out.write()
+print("listo")
